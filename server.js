2cf11270ae1c7f19157dab51a9834cab6f9d1bbb
lisahendricks <lisamariehendricks89@gmail.com>
2026-09-09 09:16:34 -0500

    Add files via upload
    * IMPORTANT: the following files are NOT included:
    node_modules, package-lock.json
    
    This is the website as of 9/9/26. Included is:
    - A basic HTML and CSS
    - A functional nav bar
    - An index page with a table, ready to be linked to the database for Week #2

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
